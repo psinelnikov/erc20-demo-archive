b1985dd703827a98f90c1f3ecc1c7d9378ed8db0
Pavel Sinelnikov <pavel.sinelnikov@outlook.com>
2021-03-24 23:12:46 -0400

    Replaced classes with React Hooks

--- client/src/pages/MetaMaskPage.jsx
+++ client/src/pages/MetaMaskPage.jsx
@@ -1,36 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Web3 from 'web3';
 import ERC20Contract from '../contracts/ERC20.json';
+import useFormInput from '../components/UseFormInput';
 
-export default class MetaMaskPage extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			web3: null,
-			contract: null,
-			receiverAddress: '',
-			receiverBalance: 0,
-			senderAddress: '',
-			senderBalance: 0,
-			amount: 0,
-			tokenName: '',
-			tokenSymbol: '',
-		};
-	}
+export default function MetaMaskPage() {
+	const [web3] = useState(new Web3(window.ethereum));
+	const [contract, setContract] = useState(null);
+	const [receiverAddress, recieverAddressInput] = useFormInput({
+		type: 'text',
+		placeholder: 'Enter the receiver address',
+	});
+	const [receiverBalance, setReceiverBalance] = useState(0);
+	const [senderAddress, senderAddressInput, setSenderAddress] = useFormInput({
+		type: 'text',
+		placeholder: 'Enter the sender address',
+	});
+	const [senderBalance, setSenderBalance] = useState(0);
+	const [amount, amountInput] = useFormInput({
+		defaultValue: 1,
+		type: 'text',
+		placeholder: 'Amount to send',
+	});
+	const [tokenName, setTokenName] = useState('');
+	const [tokenSymbol, setTokenSymbol] = useState('');
+	const [message, setMessage] = useState('');
 
-	checkBalance = async () => {
-		const { web3, senderAddress, receiverAddress, contract } = this.state;
+	useEffect(() => {
+		try {
+			const fetchData = async () => {
+				const [account] = await web3.eth.getAccounts();
+				const network = await web3.eth.net.getId();
+
+				const contract = new web3.eth.Contract(
+					ERC20Contract.abi,
+					ERC20Contract.networks[network].address
+				);
+
+				setContract(contract);
+				setTokenName(await contract.methods.name().call());
+				setTokenSymbol(await contract.methods.symbol().call());
+
+				setSenderAddress(account);
+				setSenderBalance(
+					await contract.methods.balanceOf(account).call()
+				);
+			};
+
+			fetchData();
+		} catch (error) {
+			// Catch any errors for any of the above operations.
+			alert(
+				`Failed to load web3, accounts, or contract. Check console for details.`
+			);
+			console.error(error);
+		}
+	}, [web3.eth, setSenderAddress]);
+
+	async function checkBalance() {
 		let senderBalance = 0;
 		let receiverBalance = 0;
 		let message = '';
 
 		if (senderAddress) {
 			if (web3.utils.isAddress(senderAddress)) {
-				senderBalance = await contract.methods.balanceOf(senderAddress).call();
+				senderBalance = await contract.methods
+					.balanceOf(senderAddress)
+					.call();
 			} else {
 				message = 'Error: Incorrect ethereum address for the sender';
 			}
@@ -46,168 +85,109 @@ export default class MetaMaskPage extends Component {
 			}
 		}
 
-		this.setState({ senderBalance, receiverBalance, message });
-	};
+		setMessage(message);
+		setReceiverBalance(receiverBalance);
+		setSenderBalance(senderBalance);
+	}
 
-	sendTransfer = async () => {
-		const {
-			web3,
-			senderAddress,
-			senderBalance,
-			receiverAddress,
-			amount,
-			contract,
-		} = this.state;
-
+	async function sendTransfer() {
 		if (senderBalance >= 0) {
 			try {
-				await contract.methods.transfer(receiverAddress).send(
-					{
-						from: senderAddress,
-						value: web3.utils.toWei(amount.toString(), 'ether'),
-					},
-					(error, result) => {
+				await contract.methods
+					.transfer(receiverAddress, amount)
+					.send({ from: senderAddress }, (error, result) => {
 						if (error) {
-							this.setState({ message: 'Error: ' + error.message });
+							setMessage('Error: ' + error.message);
 						} else {
-							this.setState({
-								message: 'Transaction Hash: ' + result,
-							});
+							setMessage('Transaction Hash: ' + result);
 						}
-					}
-				);
+					});
 			} catch (error) {
-				this.setState({ message: 'Error: The transfer has failed' });
+				setMessage('Error: The transfer has failed ' + error);
 			}
 		}
-	};
+	}
 
-	onChange = (e) => {
-		this.setState({ [e.target.name]: e.target.value });
-	};
+	return (
+		<>
+			<Row>
+				<Col>
+					<h1>{tokenName} Token</h1>
+				</Col>
+			</Row>
+			<Row>
+				<Col>{message && <b>{message}</b>}</Col>
+			</Row>
 
-	componentDidMount = async () => {
-		try {
-			const web3 = new Web3(window.ethereum);
-			const [senderAddress] = await web3.eth.getAccounts();
-			const network = await web3.eth.net.getId();
-			const contract = new web3.eth.Contract(
-				ERC20Contract.abi,
-				ERC20Contract.networks[network].address
-			);
-			const tokenName = await contract.methods.name().call();
-			const tokenSymbol = await contract.methods.symbol().call();
-
-			this.setState({ web3, senderAddress, contract, tokenName, tokenSymbol });
-		} catch (error) {
-			// Catch any errors for any of the above operations.
-			alert(
-				`Failed to load web3, accounts, or contract. Check console for details.`
-			);
-			console.error(error);
-		}
-	};
-
-	render() {
-		return (
-			<>
+			<Form>
 				<Row>
-					<Col>
-						<h1>{this.state.tokenName} Token</h1>
+					<Col md={6}>
+						<Form.Group controlId="formSenderAddress">
+							<Form.Label>Sender Address</Form.Label>
+							{senderAddressInput}
+						</Form.Group>
+					</Col>
+					<Col md={6}>
+						<Form.Group controlId="formReceiverAddress">
+							<Form.Label>Receiver Address</Form.Label>
+							{recieverAddressInput}
+						</Form.Group>
 					</Col>
 				</Row>
 				<Row>
-					<Col>{this.state.message && <b>{this.state.message}</b>}</Col>
+					<Col md={6}>
+						<Form.Label>Amount</Form.Label>
+						{amountInput}
+					</Col>
+					<Col md={3}>
+						<Form.Label>Sender Balance</Form.Label>
+						<Form.Control
+							plaintext
+							readOnly
+							value={`${senderBalance} ${tokenSymbol}`}
+						/>
+					</Col>
+					<Col md={3}>
+						<Form.Label>Receiver Balance</Form.Label>
+						<Form.Control
+							plaintext
+							readOnly
+							value={`${receiverBalance} ${tokenSymbol}`}
+						/>
+					</Col>
 				</Row>
-
-				<Form>
-					<Row>
-						<Col md={6}>
-							<Form.Group controlId="formSenderAddress">
-								<Form.Label>Sender Address</Form.Label>
-								<Form.Control
-									name="senderAddress"
-									value={this.state.senderAddress}
-									type="text"
-									placeholder="Enter the sender address"
-									required
-									onChange={this.onChange}
-								/>
-							</Form.Group>
-						</Col>
-						<Col md={6}>
-							<Form.Group controlId="formReceiverAddress">
-								<Form.Label>Receiver Address</Form.Label>
-								<Form.Control
-									name="receiverAddress"
-									value={this.state.receiverAddress}
-									type="text"
-									placeholder="Enter the receiver address"
-									required
-									onChange={this.onChange}
-								/>
-							</Form.Group>
-						</Col>
-					</Row>
-					<Row>
-						<Col md={6}>
-							<Form.Label>Amount</Form.Label>
-							<Form.Control
-								name="amount"
-								value={this.state.amount}
-								type="text"
-								className="u-full-width"
-								placeholder="Amount to send"
-								required
-								onChange={this.onChange}
-							/>
-						</Col>
-						<Col md={3}>
-							<Form.Label>Sender Balance</Form.Label>
-							<Form.Control
-								plaintext
-								readOnly
-								value={`${this.state.senderBalance} ${this.state.tokenSymbol}`}
-							/>
-						</Col>
-						<Col md={3}>
-							<Form.Label>Receiver Balance</Form.Label>
-							<Form.Control
-								plaintext
-								readOnly
-								value={`${this.state.receiverBalance} ${this.state.tokenSymbol}`}
-							/>
-						</Col>
-					</Row>
-					<Row>
-						<Col md={6} className="mt-4">
-							<Button variant="secondary" onClick={this.checkBalance} block>
-								Check Balance
-							</Button>
-						</Col>
-						<Col md={3} className="mt-4">
-							<Button
-								variant="primary"
-								disabled={!this.state.receiverAddress}
-								onClick={this.sendTransfer}
-								block
-							>
-								Allow
-							</Button>
-						</Col>
-						<Col md={3} className="mt-4">
-							<Button
-								variant="primary"
-								disabled={!this.state.receiverAddress}
-								onClick={this.sendTransfer}
-								block
-							>
-								Transfer
-							</Button>
-						</Col>
-					</Row>
-				</Form>
-			</>
-		);
-	}
+				<Row>
+					<Col md={6} className="mt-4">
+						<Button
+							variant="secondary"
+							onClick={checkBalance}
+							block
+						>
+							Check Balance
+						</Button>
+					</Col>
+					<Col md={3} className="mt-4">
+						<Button
+							variant="primary"
+							disabled={!receiverAddress}
+							onClick={sendTransfer}
+							block
+						>
+							Allow
+						</Button>
+					</Col>
+					<Col md={3} className="mt-4">
+						<Button
+							variant="primary"
+							disabled={!receiverAddress}
+							onClick={sendTransfer}
+							block
+						>
+							Transfer
+						</Button>
+					</Col>
+				</Row>
+			</Form>
+		</>
+	);
 }
--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -6,90 +6,134 @@ import "./IERC20Metadata.sol";
 import "./IERC20.sol";
 
 contract ERC20 is IERC20, IERC20Metadata {
-    mapping (address => uint256) private _balances;
+	mapping(address => uint256) private _balances;
 
-    mapping (address => mapping (address => uint256)) private _allowances;
+	mapping(address => mapping(address => uint256)) private _allowances;
 
-    uint256 private _totalSupply;
+	uint256 private _totalSupply;
 
-    string private _name;
-    string private _symbol;
+	string private _name;
+	string private _symbol;
 
-    constructor (string memory name_, string memory symbol_, address recipient) {
-        _name = name_;
-        _symbol = symbol_;
-		  _mint(recipient, 1000000);
-    }
+	constructor(
+		string memory name_,
+		string memory symbol_,
+		address recipient
+	) {
+		_name = name_;
+		_symbol = symbol_;
+		_mint(recipient, 1000000);
+	}
 
-    function name() public view virtual override returns (string memory) {
-        return _name;
-    }
+	function name() public view virtual override returns (string memory) {
+		return _name;
+	}
 
-    function symbol() public view virtual override returns (string memory) {
-        return _symbol;
-    }
+	function symbol() public view virtual override returns (string memory) {
+		return _symbol;
+	}
 
-    function decimals() public view virtual override returns (uint8) {
-        return 18;
-    }
+	function decimals() public view virtual override returns (uint8) {
+		return 18;
+	}
 
-    function totalSupply() public view virtual override returns (uint256) {
-        return _totalSupply;
-    }
+	function totalSupply() public view virtual override returns (uint256) {
+		return _totalSupply;
+	}
 
-    function balanceOf(address account) public view virtual override returns (uint256) {
-        return _balances[account];
-    }
+	function balanceOf(address account)
+		public
+		view
+		virtual
+		override
+		returns (uint256)
+	{
+		return _balances[account];
+	}
 
-    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-        _transfer(msg.sender, recipient, amount);
-        return true;
-    }
+	function transfer(address recipient, uint256 amount)
+		public
+		virtual
+		override
+		returns (bool)
+	{
+		_transfer(msg.sender, recipient, amount);
+		return true;
+	}
 
-    function allowance(address owner, address spender) public view virtual override returns (uint256) {
-        return _allowances[owner][spender];
-    }
+	function allowance(address owner, address spender)
+		public
+		view
+		virtual
+		override
+		returns (uint256)
+	{
+		return _allowances[owner][spender];
+	}
 
-    function approve(address spender, uint256 amount) public virtual override returns (bool) {
-        _approve(msg.sender, spender, amount);
-        return true;
-    }
+	function approve(address spender, uint256 amount)
+		public
+		virtual
+		override
+		returns (bool)
+	{
+		_approve(msg.sender, spender, amount);
+		return true;
+	}
 
-    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
-        _transfer(sender, recipient, amount);
+	function transferFrom(
+		address sender,
+		address recipient,
+		uint256 amount
+	) public virtual override returns (bool) {
+		_transfer(sender, recipient, amount);
 
-        uint256 currentAllowance = _allowances[sender][msg.sender];
-        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
-        _approve(sender, msg.sender, currentAllowance - amount);
+		uint256 currentAllowance = _allowances[sender][msg.sender];
+		require(
+			currentAllowance >= amount,
+			"ERC20: transfer amount exceeds allowance"
+		);
+		_approve(sender, msg.sender, currentAllowance - amount);
 
-        return true;
-    }
+		return true;
+	}
 
-    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
+	function _transfer(
+		address sender,
+		address recipient,
+		uint256 amount
+	) internal virtual {
+		require(sender != address(0), "ERC20: transfer from the zero address");
+		require(recipient != address(0), "ERC20: transfer to the zero address");
 
-        uint256 senderBalance = _balances[sender];
-        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
-        _balances[sender] = senderBalance - amount;
-        _balances[recipient] += amount;
+		uint256 senderBalance = _balances[sender];
+		require(
+			senderBalance >= amount,
+			"ERC20: transfer amount exceeds balance"
+		);
+		_balances[sender] = senderBalance - amount;
+		_balances[recipient] += amount;
 
-        emit Transfer(sender, recipient, amount);
-    }
+		emit Transfer(sender, recipient, amount);
+	}
 
-    function _mint(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: mint to the zero address");
+	function _mint(address account, uint256 amount) internal virtual {
+		require(account != address(0), "ERC20: mint to the zero address");
 
-        _totalSupply += amount;
-        _balances[account] += amount;
-        emit Transfer(address(0), account, amount);
-    }
+		_totalSupply += amount;
+		_balances[account] += amount;
+		emit Transfer(address(0), account, amount);
+	}
 
-    function _approve(address owner, address spender, uint256 amount) internal virtual {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
+	function _approve(
+		address owner,
+		address spender,
+		uint256 amount
+	) internal virtual {
+		require(owner != address(0), "ERC20: approve from the zero address");
+		require(spender != address(0), "ERC20: approve to the zero address");
 
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
+		_allowances[owner][spender] = amount;
+		emit Approval(owner, spender, amount);
+	}
 }
